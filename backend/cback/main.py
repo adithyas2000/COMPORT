9680babc034ac73ffdacb5a7a4ed717cf978ec1b
adithyas2000 <adithyasaranga@gmail.com>
2022-06-04 00:35:30 +0530

    Connected MongoDB
    Installed PyMongo and connected to DB

--- backend/cback/main.py
+++ backend/cback/main.py
@@ -3,6 +3,8 @@
 import selenium
 from flask import Flask, jsonify,request
 from flask_cors import CORS
+import pymongo
+
 app=Flask(__name__)
 cors=CORS(app)
 
@@ -20,5 +22,22 @@ def search():
         req=request.args.get('a')
         print("REQ : "+req)
         return jsonify(a=req)
+
+@app.route('/testmongo/')
+def mongoConnect():
+    
+    client = pymongo.MongoClient("mongodb+srv://comportadmin:comportadmin123@comportcluster.kgno3pz.mongodb.net/?retryWrites=true&w=majority")
+    db = client.u0001
+    coll1=db.searchHistory
+    data={
+        "stext":"testval",
+        "items":"someitems"
+    }
+    dataId=coll1.insert_one(data).inserted_id
+    dataId
+
+    print(db)
+    return 'LOL'
+
 if __name__=='__main__':
     app.run()
