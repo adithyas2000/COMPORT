5ad34a5ca16852bc7502abc65b3c0403887e7425
adithyas2000 <adithyasaranga@gmail.com>
2022-06-08 02:41:11 +0530

    Conditionally call scrapers

--- backend/cback/main.py
+++ backend/cback/main.py
@@ -1,15 +1,19 @@
 
 # from requests import request
 # import selenium
-import kellsScraper as keels
+from math import fabs
+import kellsScraper as keells
+import foodcityScraper as fcity
 from flask import Flask, jsonify,request
 from flask_cors import CORS
 import pymongo
 
+chrome_path=r"backend/cback/chromeWebdriver/chromedriver.exe"
+
 def getDataDict(dataobj):
     data={}
     for i in range(dataobj.lsize):
-        darray=[dataobj.ilist[i],dataobj.plist[i]]
+        darray=[dataobj.ilist[i],dataobj.plist[i],dataobj.urllist[i]]
         data[str(i)]=darray
         print(dataobj.ilist[i]+" -- "+dataobj.plist[i])
     data['size']=dataobj.lsize
@@ -32,12 +36,41 @@ def viewData(val1):
 @app.route('/search',methods=['GET','POST'])
 def search():
     if request.method == 'GET':
-        req=request.args.get('sitem')
-        # print("REQ : "+req)
-        prodDet=keels.scrape(req)
-        prodDict=getDataDict(prodDet)
+        if(request.args.get('shop1').lower()=='true'):
+            shop1=True
+        else:
+            shop1=False
 
-        return jsonify(prodDict)
+        if(request.args.get('shop2').lower()=='true'):
+            shop2=True
+        else:
+            shop2=False
+
+        if(request.args.get('shop2').lower()=='true'):
+            shop3=True
+        else:
+            shop3=False
+
+
+        stext=request.args.get('sitem')
+        
+        keellsProdDict={"NULL":"NULL"}
+        if(shop1):
+            keellsProdDet=keells.scrape(stext,chrome_path)
+            keellsProdDict=getDataDict(keellsProdDet)
+
+        fcProdDict={"NULL":"NULL"}
+        if(shop2):
+            fcProdDet=fcity.scrape(stext,chrome_path)
+            fcProdDict=getDataDict(fcProdDet)
+
+        arpicoDict={"NULL":"NULL"}
+        if(shop3):
+            # Shop3 scraper
+            # Create shop3 dictionary
+            pass
+        finalDict=[keellsProdDict,fcProdDict,arpicoDict]
+        return jsonify(finalDict)
 
 @app.route('/testmongo/')
 def mongoConnect():
